1fbcf3aac042c285bb3490b9d476be8e59125e69
Stephen <questioillustro@gmail.com>
2019-12-11 20:40:30 -0500

    Initial project create

---
+++ src/test/java/com/brewster/financialplanning/FinancialPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewster.financialplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialPlanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
